e082737a48a4688265c82962b4747a5f72ad3f64
Adarsh Kumar <adarshkumar2731@gmail.com>
2025-08-26 14:54:47 +0530

    Use dynamic port and add start script

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,5 @@
-require('dotenv').config()
-const app = require('./src/app.js')
+const port = process.env.PORT || 3000;
 
-
-
-app.listen(3000,() => {
-console.log('Server is running on http://localhost:3000')
-
-} )
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
