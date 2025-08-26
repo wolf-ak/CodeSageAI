fa0acfaaebb17f521e157aff7954a9c1037b036b
Adarsh Kumar <adarshkumar2731@gmail.com>
2025-08-26 21:09:07 +0530

    minor changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,7 @@
-const port = process.env.PORT || 3000;
+require('dotenv').config()
+const app = require('./src/app.js')
+
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on http://localhost:${port}`)
